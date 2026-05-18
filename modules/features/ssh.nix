47fd0672021cfbf7c95825986f4f60ea79c6bf96
sean tietz <sean.tietz2@gmail.com>
2026-05-18 23:51:53 +0200

    add github to known hosts declaratively

--- modules/features/ssh.nix
+++ modules/features/ssh.nix
@@ -1,16 +1,25 @@
 { ... }:
 {
-  flake.modules.homeManager.ssh = {
-    programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
+  flake.modules.homeManager.ssh =
+    { config, ... }:
+    {
+      programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
 
-      matchBlocks = {
-        "github.com" = {
-          user = "git";
-          identityFile = "~/.ssh/id_ed25519";
+        matchBlocks = {
+          "github.com" = {
+            user = "git";
+            identityFile = "~/.ssh/id_ed25519";
+          };
         };
       };
+
+      home.file."${config.home.homeDirectory}/.ssh/known_hosts" = {
+        text = ''
+          github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
+        '';
+        force = true;
+      };
     };
-  };
 }
