a5627badedad835635839bbbacccf62b45cf213f
sean tietz <sean.tietz2@gmail.com>
2026-05-19 13:05:14 +0200

    switch to v3

--- modules/features/cachyos-kernel.nix
+++ modules/features/cachyos-kernel.nix
@@ -11,13 +11,13 @@
     {
       nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.default ];
 
-      boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v4;
+      boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v3;
 
       nix.settings = {
-        substituters = [
+        extra-substituters = [
           "https://attic.xuyh0120.win/lantian"
         ];
-        trusted-public-keys = [
+        extra-trusted-public-keys = [
           "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
         ];
       };
