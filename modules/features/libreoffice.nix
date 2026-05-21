f3648b903f125f6eadef3cb22f439dbbaa4bf4e5
sean tietz <sean.tietz2@gmail.com>
2026-05-21 13:18:54 +0200

    create libreoffice module and hide unneeded desktop entries

---
+++ modules/features/libreoffice.nix
@@ -0,0 +1,32 @@
+{ ... }: {
+  flake.modules.homeManager.libreoffice = { pkgs, ... }: {
+    home.packages = [ pkgs.libreoffice-fresh ];
+
+    xdg.dataFile = {
+      "applications/base.desktop".text = ''
+        [Desktop Entry]
+        Hidden=true
+      '';
+      "applications/draw.desktop".text = ''
+        [Desktop Entry]
+        Hidden=true
+      '';
+      "applications/impress.desktop".text = ''
+        [Desktop Entry]
+        Hidden=true
+      '';
+      "applications/math.desktop".text = ''
+        [Desktop Entry]
+        Hidden=true
+      '';
+      "applications/startcenter.desktop".text = ''
+        [Desktop Entry]
+        Hidden=true
+      '';
+      "applications/xsltfilter.desktop".text = ''
+        [Desktop Entry]
+        Hidden=true
+      '';
+    };
+  };
+}
