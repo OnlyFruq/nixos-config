8cd66f59db0754b5546364c58911b0d3dcc18659
OnlyFruq <123996207+OnlyFruq@users.noreply.github.com>
2026-07-09 23:07:26 +0200

    Add files via upload

---
+++ modules/hosts/minipc.nix
@@ -0,0 +1,46 @@
+{ inputs, ... }:
+{
+  flake.modules.nixos.minipc =
+    { ... }:
+    {
+      imports = with inputs.self.modules.nixos; [
+        # base + mechanisms (every host)
+        hostDefault
+        disko
+        persistence
+        user-groups
+        # user + machine capability
+        frugzzz1
+        desktop
+      ];
+
+      networking.hostName = "minipc";
+
+      diskoCfg = {
+        device = "/dev/disk/by-id/CHANGE-ME"; # TODO: echtes Device nach lsblk/by-id eintragen
+        swapSize = "10G";
+        encrypt = false; # kein LUKS-Prompt beim Boot, wie in der VM
+      };
+
+      # Typische Module für ein physisches Gerät mit NVMe/SATA + USB-Bootmedium.
+      # Nach dem ersten echten Boot ggf. anhand von `nixos-generate-config` prüfen.
+      boot.initrd.availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "nvme"
+        "usb_storage"
+        "sd_mod"
+      ];
+
+      # HP Pro Mini 400 G9: Intel Core (12./13./14. Gen), Intel UHD Graphics 770,
+      # Intel AX211 WiFi 6E + Bluetooth 5.2 (bei WLAN-Ausstattung).
+      hardware.cpu.intel.updateMicrocode = true;
+      hardware.graphics.enable = true;
+      hardware.enableRedistributableFirmware = true; # Firmware für den WiFi-Chip
+      hardware.bluetooth.enable = true;
+
+      hostCfg.audio.enable = true;
+    };
+
+  flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" "minipc";
+}
